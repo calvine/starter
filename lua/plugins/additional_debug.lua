9387b7ac8d0c1957b97f2bc84b75e5820bfe6bb9
CalvinE <calvin.echols@gmail.com>
2025-11-19 17:23:59 -0500

    Adding custom debugging profiles for go.

---
+++ lua/plugins/additional_debug.lua
@@ -0,0 +1,25 @@
+local dap = require("dap")
+
+if not dap.configurations.go then
+  dap.configurations.go = {}
+end
+
+table.insert(dap.configurations["go"], {
+  type = "delve",
+  name = "Delve: Debug File",
+  request = "launch",
+  program = "${fileDirname}",
+  cwd = "${workspaceFolder}",
+})
+table.insert(dap.configurations["go"], {
+  type = "delve",
+  name = "Delve: Debug File With Arguments",
+  request = "launch",
+  program = "${fileDirname}",
+  cwd = "${workspaceFolder}",
+  args = function()
+    return vim.split(vim.fn.input("Args: "), " ")
+  end,
+})
+
+return {}
